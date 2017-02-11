bb83214c922c436e938cfbd6257190458b7926f8
Eric Pederson <ericacm+github@gmail.com>
2017-02-11 02:12:44 -0500

    Add .gitignore

--- thread_pool_quick_sort.cpp
+++ thread_pool_quick_sort.cpp
@@ -1,8 +1,15 @@
 #include <iostream>
+#include <random>
 #include "parallel_quick_sort.h"
 
 int main() {
-    std::list<int> list{56, 1, 2, 7, 23, 4, 22};
+    std::mt19937 rng;
+    rng.seed(std::random_device()());
+    std::uniform_int_distribution<std::mt19937::result_type> dist(1,1000);
+
+    std::list<int> list;
+    for (int i = 0; i < 50; ++i) list.push_back(dist(rng));
+
     auto res = parallel_quick_sort(list);
 
     for (auto &elem : res) {
